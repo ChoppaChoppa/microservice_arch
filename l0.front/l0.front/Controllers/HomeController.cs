88f233dff4bca2a9a8a088a78f4f07d4e0460cd9
mora <gopnik0770@mail.ru>
2022-03-06 18:19:48 +0300

    asp.net

---
+++ l0.front/l0.front/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using l0.front.Models;
+using System.Net;
+using System.IO;
+using System.Text.Json;
+
+namespace l0.front.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetByID(string ID)
+        {
+            WebRequest request = WebRequest.Create($"http://127.0.0.1/sub_cache/" + Request.Form["id_input"]);
+            request.Method = "GET";
+
+            WebResponse response = await request.GetResponseAsync();
+            string respJson;
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    respJson = reader.ReadToEnd();
+                }
+            }
+
+            var orderInfo = JsonSerializer.Deserialize<OrderInfo>(respJson);
+
+            return View("Order", orderInfo);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ l0.front/l0.front/Models/OrderInfoViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace l0.front.Models
+{
+    public class OrderInfo
+    {
+        public string order_uid { get; set; }
+        public string track_number { get; set; }
+        public string entry { get; set; }
+        public Delivery delivery { get; set; }
+        public Payment payment { get; set; }
+        public IList<Item> items { get; set; }
+        public string locale { get; set; }
+        public string internal_signature { get; set; }
+        public string customer_id { get; set; }
+        public string delivery_service { get; set; }
+        public string shardkey { get; set; }
+        public int sm_id { get; set; }
+        public DateTime date_created { get; set; }
+        public string oof_shard { get; set; }
+
+        public struct Delivery
+        {
+            public string name { get; set; }
+            public string phone { get; set; }
+            public string zip { get; set; }
+            public string city { get; set; }
+            public string address { get; set; }
+            public string region { get; set; }
+            public string email { get; set; }
+        }
+
+        public class Payment
+        {
+            public string transaction { get; set; }
+            public string request_id { get; set; }
+            public string currency { get; set; }
+            public string provider { get; set; }
+            public int amount { get; set; }
+            public int payment_dt { get; set; }
+            public string bank { get; set; }
+            public int delivery_cost { get; set; }
+            public int goods_total { get; set; }
+            public int custom_fee { get; set; }
+        }
+
+        public class Item
+        {
+            public int chrt_id { get; set; }
+            public string track_number { get; set; }
+            public int price { get; set; }
+            public string rid { get; set; }
+            public string name { get; set; }
+            public int sale { get; set; }
+            public string size { get; set; }
+            public int total_price { get; set; }
+            public int nm_id { get; set; }
+            public string brand { get; set; }
+            public int status { get; set; }
+        }
+    }
+}
