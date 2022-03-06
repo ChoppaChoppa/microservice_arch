8a8f505e90981f47fa9d648908b1ede34a8e43d3
mora <gopnik0770@mail.ru>
2022-03-07 00:57:21 +0300

    accessing the database through another service

--- subscriber_db/PgDataBase/pg.go
+++ subscriber_db/PgDataBase/pg.go
@@ -115,7 +115,7 @@ func (dataBase *DB) Add(ctx context.Context, order Models.OrderInfo) (Models.Ord
 	return order, nil
 }
 
-func (dataBase *DB) Get(ctx context.Context, id string) (Models.OrderInfo, error) {
+func (dataBase *DB) GetOrder(ctx context.Context, id string) (Models.OrderInfo, error) {
 	var order Models.OrderInfo
 
 	if errGetOrder := dataBase.conn.GetContext(ctx, &order, `
@@ -154,6 +154,61 @@ func (dataBase *DB) Get(ctx context.Context, id string) (Models.OrderInfo, error
 	}
 
 	return order, nil
+}
+
+func (dataBase *DB) GetLasts(ctx context.Context, count int) ([]Models.OrderInfo, error){
+	var Orders []Models.OrderInfo
+
+	fmt.Println("start get lasts")
+	if errGetOrder := dataBase.conn.SelectContext(ctx, &Orders, `
+			SELECT id, order_uid, track_number, entry, locale, internal_signature,
+                        customer_id, delivery_service, shard_key, sm_id, date_created, oof_shard
+			FROM order_info
+			ORDER BY id DESC LIMIT $1`, count);
+		errGetOrder != nil {
+		fmt.Println("get order ", errGetOrder)
+		return nil, fmt.Errorf("get order: %v", errGetOrder)
+	}
+
+	fmt.Println("orders got", Orders)
+
+	for i, _ := range Orders {
+		if errGetDelivery := dataBase.conn.GetContext(ctx, &Orders[i].Delivery, `
+			SELECT name, phone, zip, city, address, region, email
+			FROM deliver
+			WHERE order_id = $1`, Orders[i].ID);
+			errGetDelivery != nil {
+			fmt.Println("get delivery ", errGetDelivery)
+			return nil, fmt.Errorf("get delivery: %v", errGetDelivery)
+		}
+
+		fmt.Println("delivery got")
+
+		if errGetPayments := dataBase.conn.GetContext(ctx, &Orders[i].Payment, `
+			SELECT transaction, request_id, currency, provider, amount,
+                    payment_dt, bank, delivery_cost, goods_total, custom_fee
+			FROM payment
+			WHERE order_id = $1`, Orders[i].ID);
+			errGetPayments != nil {
+			fmt.Println("get payments ", errGetPayments)
+			return nil, fmt.Errorf("get payments: %v", errGetPayments)
+		}
+
+		fmt.Println("payments got ", Orders[i].Payment)
+
+		if errGetItems := dataBase.conn.SelectContext(ctx, &Orders[i].Items, `
+			SELECT chrt_id, track_number, price, rid, name, sale, size, total_price, nm_id, brand, status
+			FROM item
+			WHERE order_id = $1`, Orders[i].ID);
+			errGetItems != nil {
+			fmt.Println("get items ", errGetItems)
+			return nil, fmt.Errorf("get items: %v", errGetItems)
+		}
+
+		fmt.Println("items got ", Orders[i].Items)
+	}
+
+	return Orders, nil
 }
 
 func (dataBase *DB) InTx(ctx context.Context, isolation sql.IsolationLevel, f func(tx sqlx.Tx) error) error {
--- subscriber_db/Route/router.go
+++ subscriber_db/Route/router.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"io/ioutil"
 	"net/http"
 
 	"sub_db/HttpProcessing"
@@ -12,7 +13,8 @@ import (
 )
 
 type IPgDataBase interface {
-	Get(ctx context.Context, id string) (Models.OrderInfo, error)
+	GetOrder(context.Context, string) (Models.OrderInfo, error)
+	GetLasts(context.Context, int)    ([]Models.OrderInfo, error)
 }
 
 type DataBase struct {
@@ -23,25 +25,62 @@ func Router(pg DataBase) *chi.Mux {
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
 
-	router.Get("/{id}", func(w http.ResponseWriter, r *http.Request) {
-		id := chi.URLParam(r, "id")
+	router.Route("/sub_db/get", func(route chi.Router) {
+		route.Get("/{id}", func(w http.ResponseWriter, r *http.Request) {
+			id := chi.URLParam(r, "id")
 
-		order, errGet := pg.DB.Get(r.Context(), id)
-		if errGet != nil {
-			HttpPorcessing.HttpError(w, errGet, "err get",
-				"server error", http.StatusInternalServerError)
-			return
-		}
+			order, errGet := pg.DB.GetOrder(r.Context(), id)
+			if errGet != nil {
+				HttpPorcessing.HttpError(w, errGet, "err get",
+					"server error", http.StatusInternalServerError)
+				return
+			}
 
-		resp, errMarshal := json.Marshal(order)
-		if errMarshal != nil {
-			HttpPorcessing.HttpError(w, errMarshal, "err marshal",
-				"server error", http.StatusInternalServerError)
-			return
-		}
+			resp, errMarshal := json.Marshal(order)
+			if errMarshal != nil {
+				HttpPorcessing.HttpError(w, errMarshal, "err marshal",
+					"server error", http.StatusInternalServerError)
+				return
+			}
 
-		w.Header().Set("Content-Type", "application-json")
-		w.Write(resp)
+			w.Header().Set("Content-Type", "application-json")
+			w.Write(resp)
+		})
+
+		route.Get("/lasts", func(w http.ResponseWriter, r *http.Request) {
+			type request struct {
+				count int
+			}
+			var reqStruct request
+			body, errGetBody := ioutil.ReadAll(r.Body)
+			if errGetBody != nil {
+				HttpPorcessing.HttpError(w, errGetBody, "read body",
+					"bad request", http.StatusBadRequest)
+				return
+			}
+			if errUnmarshalBody := json.Unmarshal(body, &reqStruct); errUnmarshalBody != nil {
+				HttpPorcessing.HttpError(w, errUnmarshalBody, "unmarshal body",
+					"bad request", http.StatusBadRequest)
+				return
+			}
+
+			orders, errGet := pg.DB.GetLasts(r.Context(), reqStruct.count)
+			if errGet != nil {
+				HttpPorcessing.HttpError(w, errGet, "get last orders",
+					"server error", http.StatusInternalServerError)
+				return
+			}
+
+			resp, errMarshal := json.Marshal(orders)
+			if errMarshal != nil {
+				HttpPorcessing.HttpError(w, errGet, "marshal orders",
+					"server error", http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "application-json")
+			w.Write(resp)
+		})
 	})
 
 	return router
